ccefa7b8093b9389736dd8da461a44b8abe0f399
aktan2803 <81546800+aktan2803@users.noreply.github.com>
2021-10-14 11:20:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,63 @@
 <html>
 <head>
 <style>
+header {
+  padding: 10px;
+  text-align: center;
+  font-size: 35px;
+  color: Tomato;
+}
 body {
   background-image: url('ph.jpeg');
   background-repeat: no-repeat;
   background-attachment: fixed;  
   background-size: cover;
 }
+
+nav {
+  float: left;
+  width: 65%;
+  height: 500px;
+  padding: 20px;
+}
+
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+article {
+  float: right;
+  padding: 20px;
+  width: 20%;
+  height: 500px;
+}
+footer {
+  padding: 10px;
+  text-align: center;
+  color: Tomato;
+}
 </style>
 </head>
 <body>
 
-<h2>Hello I'm AKTAN</h2>
+<header>
+  <h2>Hello I'm AKTAN</h2>
+</header>
 
-<p>I'm senior year college student at IT and Busness college.</p>
+  <nav>
+    <ul>
+    <p style="color:Tomato;">I'm senior year college student at IT and Busness college.</p>
+    </ul>
+  </nav>
+
+<article>
+ <p> fasedgf </p>
+</article>
+
 
 <footer>
-  <p1>Phone Namber: +996(777)777 777</p1>
+  <p>Phone Namber: +996(777)777 777</p>
 </footer>
 
 </body>
